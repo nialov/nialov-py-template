137d84462975cd64de6e0d8ef4a9227ddea8a891
nialov <nikolasovaskainen@hotmail.com>
2021-02-21 18:53:48 +0200

    change to local copier template

--- devnoxfile.py
+++ devnoxfile.py
@@ -2,6 +2,7 @@
 Nox session tests for template.
 """
 import nox
+from pathlib import Path
 
 template_url = "https://github.com/nialov/nialov-py-template"
 scaffold_url = "https://github.com/nialov/nialov-py-template-test"
@@ -14,12 +15,13 @@ def test(session: nox.Session):
     """
     Test template with nox session.
     """
+    current_dir = Path(".").resolve()
     tmp_dir = session.create_tmp()
     session.chdir(tmp_dir)
     session.install("pipenv")
     session.run("git", "clone", scaffold_url, "--depth", "1")
     session.chdir(scaffold_dir)
-    session.run("copier", "--force", "update")
+    session.run("copier", "--force", "copy", str(current_dir), ".")
     session.run("pipenv", "--rm", success_codes=[0, 1])
     session.run("pipenv", "install", "--dev")
     session.run("pipenv", "run", "invoke", "make")
