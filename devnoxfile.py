4a99f06c54c5144ae9bf6f0759660119b480a58e
nialov <nikolasovaskainen@hotmail.com>
2021-03-22 16:34:41 +0200

    test versioneer

--- devnoxfile.py
+++ devnoxfile.py
@@ -15,13 +15,35 @@ def test(session: nox.Session):
     """
     Test template with nox session.
     """
+    # Save current dir to variable
     current_dir = Path(".").resolve()
+
+    # Create temporary directory
     tmp_dir = session.create_tmp()
+
+    # Change to the temporary directory
     session.chdir(tmp_dir)
+
+    # Install dependencies
     session.install("pipenv", "copier")
+
+    # git clone scaffold Python 3.8 project
     session.run("git", "clone", scaffold_url, "--depth", "1", external=True)
+
+    # Change to the cloned dir
     session.chdir(scaffold_dir)
+
+    # Run copier
     session.run("copier", "--force", "copy", str(current_dir), ".")
+
+    # Rm any existing pipenv
     session.run("pipenv", "--rm", success_codes=[0, 1])
+
+    # Install with pipenv
     session.run("pipenv", "install", "--dev")
+
+    # Run versioneer
+    session.run("versioneer", "install")
+
+    # Run tests that come from copier template files
     session.run("pipenv", "run", "invoke", "make")
