8323cfb52b662c6e2ec4c4509b18ec9e60bfc0e0
nialov <nikolasovaskainen@hotmail.com>
2021-04-08 22:49:39 +0300

    fix corrupt git

--- README.md
+++ README.md
@@ -3,10 +3,35 @@
 This is a [copier](https://github.com/copier-org/copier) template for
 Python 3.8 development.
 
+Highly opionated and made for personal use.
+
+## Short description of functionality
+
+-   Testing framework based on `invoke` and `nox`.
+
+-   Testing done with `pytest`, and code `coverage` is checked.
+
+    -   `coverage-badge` is currently locally generated for use in `README.rst`.
+
+-   Documentation with `sphinx`, theme `sphinx-rtd-theme`.
+
+-   Contains GitHub Actions that call the aforementioned test frameworks
+    (`lint` and `test-and-publish`).
+
+    -   By default an
+        [action](https://github.com/pypa/gh-action-pypi-publish) exists
+        that will push to PyPI on tagged commits. However this requires
+        a `PYPI_PASSWORD` API token to work.
+    -   Non-tagged commits simply try building the package without
+        pushing anywhere.
+
 ## Running tests
 
 Run tests for the template with:
 
-``` {bash}
+``` {.{bash}}
 pipenv run invoke -c devtasks test
 ```
+
+Testing uses another, scaffolded Python project at:
+<https://github.com/nialov/nialov-py-template-test>
--- devnoxfile.py
+++ devnoxfile.py
@@ -15,13 +15,42 @@ def test(session: nox.Session):
     """
     Test template with nox session.
     """
+    # Save current dir to variable
     current_dir = Path(".").resolve()
+
+    # Create temporary directory
     tmp_dir = session.create_tmp()
+
+    # Change to the temporary directory
     session.chdir(tmp_dir)
-    session.install("pipenv", "copier")
+
+    # Install dependencies
+    session.install("pipenv", "copier", "versioneer")
+
+    # git clone scaffold Python 3.8 project
     session.run("git", "clone", scaffold_url, "--depth", "1", external=True)
+
+    # Change to the cloned dir
     session.chdir(scaffold_dir)
+
+    # Run copier
     session.run("copier", "--force", "copy", str(current_dir), ".")
+
+    # Rm any existing pipenv
     session.run("pipenv", "--rm", success_codes=[0, 1])
+
+    # Install with pipenv
     session.run("pipenv", "install", "--dev")
+
+    # Update the local setup.py file
+    session.run("pipenv", "run", "invoke", "requirements")
+
+    # Run versioneer
+    session.run("versioneer", "install")
+
+    # Run tests that come from copier template files
     session.run("pipenv", "run", "invoke", "make")
+
+    # Make sure versioneer files are not documented
+    if Path("docs_src/mypackage._version.rst").exists():
+        raise FileExistsError("Expected no apidoc on _version.py file.")
