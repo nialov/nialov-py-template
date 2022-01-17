24accfca006c34058ccb5a717c4c358544b39ac7
nialov <nikolasovaskainen@hotmail.com>
2022-01-17 12:45:53 +0200

    build: add nix dev env

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    invoke
+    nox
+    python37
+    python38
+    python39
+    pre-commit
+    git
+    cacert
+  ];
+
+  shellHook = with pkgs; ''
+    echo Welcome to nix shell.
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib
+    export GIT_SSL_CAINFO=${cacert}/etc/ssl/certs/ca-bundle.crt
+    export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    export CURL_CA_BUNDLE=${cacert}/etc/ssl/certs/ca-bundle.crt
+  '';
+}
