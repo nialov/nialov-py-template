90ec540161b270344cf13f2018d89286d5ac5c7b
nialov <nikolasovaskainen@gmail.com>
2022-01-24 14:41:28 +0200

    build: reuse nix declarations

--- flake.nix
+++ flake.nix
@@ -1,18 +1,1 @@
-{
-  description = "nialov-py-template development environment";
-
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { nixpkgs, flake-utils, ... }:
-      (flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = nixpkgs.legacyPackages."${system}"; in
-        {
-          devShell = import ././shell.nix {
-            pkgs=pkgs;
-          };
-        }
-      ));
-}  
+template/flake.nix.tmpl
