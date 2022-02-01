6b99e43f1cc2563f8793a85ed9a25f70617da0e8
nialov <nikolasovaskainen@gmail.com>
2022-02-01 09:51:27 +0200

    fix(nix): add zlib

--- template/flake.nix
+++ template/flake.nix
@@ -8,53 +8,53 @@
 
   outputs = { nixpkgs, flake-utils, ... }:
     let
-      mkshell = pkgs: with pkgs; mkShell rec {
-        buildInputs = [
-          poetry
-          python38
-          python39
-          pre-commit
-          pandoc
-          git
-          cacert
-          stdenv
-          pastel
-        ];
+      mkshell = pkgs:
+        with pkgs;
+        mkShell rec {
+          buildInputs = [
+            poetry
+            python38
+            python39
+            pre-commit
+            pandoc
+            git
+            cacert
+            stdenv
+            pastel
+            zlib
+          ];
 
-        # Required for building C extensions
-        LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib";
-        # Certificates for secure connections for e.g. pip downloads
-        GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-        SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-        CURL_CA_BUNDLE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-        # Required to fully use the python environments
-        PYTHON38PATH = "${python38}/lib/python3.8/site-packages";
-        # PYTHONPATH is overridden with contents from e.g. poetry */site-package.
-        # We do not want them to be in PYTHONPATH.
-        # Therefore, in ./.envrc PYTHONPATH is set to the _PYTHONPATH defined below
-        # and also in shellHooks (direnv does not load shellHook exports, always).
-        _PYTHONPATH = "${PYTHON38PATH}:${python39}/lib/python3.9/site-packages";
+          # Required for building C extensions
+          LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:${zlib}/lib";
+          # Certificates for secure connections for e.g. pip downloads
+          GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+          SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+          CURL_CA_BUNDLE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+          # Required to fully use the python environments
+          PYTHON38PATH = "${python38}/lib/python3.8/site-packages";
+          # PYTHONPATH is overridden with contents from e.g. poetry */site-package.
+          # We do not want them to be in PYTHONPATH.
+          # Therefore, in ./.envrc PYTHONPATH is set to the _PYTHONPATH defined below
+          # and also in shellHooks (direnv does not load shellHook exports, always).
+          _PYTHONPATH =
+            "${PYTHON38PATH}:${python39}/lib/python3.9/site-packages";
 
-        envrc_contents = ''
-          use flake
-          export PYTHONPATH=$_PYTHONPATH
-        '';
+          envrc_contents = ''
+            use flake
+            export PYTHONPATH=$_PYTHONPATH
+          '';
 
-        shellHook = ''
-          [[ -a .pre-commit-config.yaml ]] && \
-            echo "Installing pre-commit hooks"; pre-commit install
-          pastel paint -n green "
-          Run poetry install to install environment from poetry.lock
-          "
-          export PYTHONPATH=$_PYTHONPATH
-          [[ ! -a .envrc ]] && echo -n "$envrc_contents" > .envrc
-        '';
-      };
-    in
-    (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages."${system}"; in
-      {
-        devShell = mkshell pkgs;
-      }
-    ));
-}  
+          shellHook = ''
+            [[ -a .pre-commit-config.yaml ]] && \
+              echo "Installing pre-commit hooks"; pre-commit install
+            pastel paint -n green "
+            Run poetry install to install environment from poetry.lock
+            "
+            export PYTHONPATH=$_PYTHONPATH
+            [[ ! -a .envrc ]] && echo -n "$envrc_contents" > .envrc
+          '';
+        };
+    in (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in { devShell = mkshell pkgs; }));
+}
